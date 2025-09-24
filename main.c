947abf63ce13541a22aa662d0586e2599c0b5d3f
ignaciosiva <ignsilva2023@udec.cl>
2025-09-24 01:29:22 -0300

    Implementacion miprof ejec y ejecsave

--- main.c
+++ main.c
@@ -47,6 +47,7 @@ int main() {
             continue;
         }
 
+        // Comando interno cd
         if (strcmp(args[0], "cd") == 0){
             if (args[1] == NULL){
                 perror("cd: Faltan argumentos");
@@ -57,7 +58,64 @@ int main() {
             free_args(args);
             continue;
         }
+  
+        if (strcmp(args[0], "miprof") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "Uso: miprof [ejec|ejecsave archivo] comando args...\n");
+                free_args(args);
+                continue;
+            }
 
+            int save = 0;
+            char *filename = NULL;
+            int cmd_index = 2; // índice del comando a ejecutar
+
+            if (strcmp(args[1], "ejec") == 0) {
+                save = 0;
+                cmd_index = 2;
+            } 
+            else if (strcmp(args[1], "ejecsave") == 0) {
+                if (args[2] == NULL) {
+                    fprintf(stderr, "Uso: miprof ejecsave archivo comando args...\n");
+                    free_args(args);
+                    continue;
+                }
+                save = 1;
+                filename = args[2];
+                cmd_index = 3;
+            } 
+            else {
+                fprintf(stderr, "Opción inválida: %s\n", args[1]);
+                free_args(args);
+                continue;
+            }
+
+            pid_t pid = fork();
+            if (pid == 0) {
+                if (save) {
+                    FILE *f = fopen(filename, "a");
+                    if (!f) {
+                        perror("No se pudo abrir el archivo");
+                        exit(EXIT_FAILURE);
+                    }
+                    dup2(fileno(f), STDOUT_FILENO);
+                    dup2(fileno(f), STDERR_FILENO);
+                    fclose(f);
+                }
+                execvp(args[cmd_index], &args[cmd_index]);
+                perror("execvp falló");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0) {
+                int status;
+                waitpid(pid, &status, 0);
+                printf("Exit status: %d\n", status);
+            } else {
+                perror("fork falló");
+            }
+
+            free_args(args);
+            continue;
+        }
         pid_t pid = fork();
         
         if (pid == 0){
@@ -68,12 +126,12 @@ int main() {
         }else if(pid > 0){
             int status;
             waitpid(pid, &status, 0);
-            printf("Exist status: %d\n", status);
-            
+            printf("Exit status: %d\n", status);
         }else{
             perror("fork failed");
         }
 
         free_args(args);
     }
+    
 }
